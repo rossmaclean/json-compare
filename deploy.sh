4e6b692ea6d59e8e721beae543740559de04baa6
Ross MacLean <ross@rossmac.co.uk>
2020-10-14 22:16:33 +0100

    Add deploy files.

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker build -t rossmaclean/json-compare:latest .
+docker push rossmaclean/json-compare:latest
+cloudron update --server my.rossmac.co.uk --app json-compare.rossmac.co.uk --image rossmaclean/json-compare:latest
